5300c5f157df621a8eed2e1d2ebfaf94ea62fa61
Hendrik Brocke <hendrikbrocke@mailbox.org>
2026-05-02 17:36:15 +0200

    CH1:L5

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
+
 def main():
-    print("Hello from aiagent!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key is None:
@@ -11,10 +12,19 @@ def main():
     
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="My Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    prompt = args.user_prompt
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents='"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."'
+        model='gemini-2.5-flash', contents=prompt
     )
-    print(response.text)
+    if response.usage_metadata is not None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  
+        print(response.text)
+    else:
+        raise RuntimeError("Usage metadata not found")
 
 if __name__ == "__main__":
     main()
