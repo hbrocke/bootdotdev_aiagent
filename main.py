0ae4355db38f86e7941f8245596f7201efcd6fec
Hendrik Brocke <hendrikbrocke@mailbox.org>
2026-05-02 17:44:19 +0200

    CH1:L7

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -15,9 +16,9 @@ def main():
     parser = argparse.ArgumentParser(description="My Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-    prompt = args.user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=prompt
+        model='gemini-2.5-flash', contents=messages
     )
     if response.usage_metadata is not None:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
