6cc044fad29a16054d0dcc4e0f56b40830c73239
Hendrik Brocke <hendrikbrocke@mailbox.org>
2026-05-02 17:52:16 +0200

    CH1:L7

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ def main():
 
     parser = argparse.ArgumentParser(description="My Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
         model='gemini-2.5-flash', contents=messages
     )
     if response.usage_metadata is not None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  
-        print(response.text)
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")  
+        print(f"Response:\n {response.text}")
     else:
         raise RuntimeError("Usage metadata not found")
 
