cbf571437d8b6b8092950f7e8bff23317c987b60
Hendrik Brocke <hendrikbrocke@mailbox.org>
2026-05-02 20:20:47 +0200

    CH2:L3

---
+++ functions/get_files_info.py
@@ -0,0 +1,23 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(working_dir_abs):
+            return f'Error: "{working_dir_abs}" is not a directory'
+    
+        files = os.listdir(target_dir) 
+        message = f"Result for { directory } directory: \n"
+        for item in files:
+            item_path = target_dir+"/"+item
+            item_isdir = os.path.isdir(item_path)
+            item_size = os.path.getsize(item_path)
+            message += f"- {item}: file_size={item_size}, is_dir={item_isdir}\n" 
+        return message
+    except Exception as e:
+        return f"Error: {e}"
